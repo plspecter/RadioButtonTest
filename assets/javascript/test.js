2ce3a78a910d7ca6e4b5a8e12b4f43a6fbbe4560
Perri Specter <plspecter@gmail.com>
2018-11-06 14:36:09 -0500

    RadioButtonTest

--- assets/javascript/test.js
+++ assets/javascript/test.js
@@ -1,10 +1,13 @@
 jQuery(document).ready(function () {
     console.log("this is ready");
 
-    var result1;
+    reset();
+
+    var userGuess1;
     var correct = 0;
     var incorrect = 0;
     var unanswered = 0;
+
 
     $("#click").on("click", function () {
 
@@ -13,20 +16,62 @@ jQuery(document).ready(function () {
         $("#unanswered").text(unanswered);
 
 
-   
-        if (result1 === $("#ice").html($("input:checked").val()))  {
-            correct++
-        } else if (result1 === $('input[#fire="inlineRadioOptions1"]:checked'))  {
-            incorrect++;
-        }
-        else if (result1 === $('input[#poison="inlineRadioOptions1"]:checked'))  {
-            incorrect++;
-        }
-        else if (result1 === $('input[#thunder="inlineRadioOptions1"]:checked')) {
-            incorrect++;
-        };
+        var answers = ['ice', 'FrozenSeaway'];
+        var wrongAnswers = ['fire', 'poison', 'thunder'];
+        userGuess1 = $("input[name='inlineRadioOptions1']:checked").val()
+        userGuess2 = $("input[name='inlineRadioOptions2']:checked").val()
 
-    
+        if (userGuess1 === answers[0])  {
+            correct++;
+            //This prints the correct answer to the html
+            $("#correct").text(correct);
+            $("#correct").html();
+            console.log(correct, + "correct")
+        } else if (userGuess1 === undefined) {
+            unanswered++
+            $("#unanswered").text(unanswered);
+            $("#unanswered").html();
+            console.log("you did not choose an answer")
+        } else {
+            incorrect++;
+            $("#incorrect").text(incorrect);
+            $("#incorrect").html();
+            console.log(incorrect, + "incorrect")
+            console.log(incorrect)
+        }
+
+        //==QUESTION 2
+
+        if (userGuess2 === answers[1])  {
+            correct++;
+            //This prints the correct answer to the html
+            $("#correct").text(correct);
+            $("#correct").html();
+            console.log(correct, + "correct")
+            console.log(incorrect)
+        } else if (userGuess1 === undefined) {
+            unanswered++
+            $("#unanswered").text(unanswered);
+            $("#unanswered").html();
+            console.log("you did not choose an answer")
+        } else {
+            incorrect++;
+            $("#incorrect").text(incorrect);
+            $("#incorrect").html();
+        }
+
+        // } else if (userGuess1 === $('input[#fire="inlineRadioOptions1"]:checked'))  {
+        //     incorrect++;
+        // }
+        // else if (userGuess1 === $('input[#poison="inlineRadioOptions1"]:checked'))  {
+        //     incorrect++;
+        // }
+        // else if (userGuess1 === $('input[#thunder="inlineRadioOptions1"]:checked')) {
+        //     incorrect++;
+        // };
+        $("#reset").on("click", function () {
+            reset();
+        })
         
         // function choice() {
         //     var type = $("radio");
